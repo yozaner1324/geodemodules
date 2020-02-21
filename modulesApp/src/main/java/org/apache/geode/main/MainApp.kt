bfec497f2a0be206fea64efc06366a8e61da180b
Udo Kohlmeyer <ukohlmeyer@pivotal.io>
2020-02-21 10:30:36 -0800

    Clean up

--- modulesApp/src/main/java/org/apache/geode/main/MainApp.kt
+++ modulesApp/src/main/java/org/apache/geode/main/MainApp.kt
@@ -3,7 +3,6 @@ package org.apache.geode.main
 import org.apache.geode.module.service.ModuleService
 import org.apache.geode.module.service.impl.JBossModuleServiceImpl
 import org.apache.geode.service.SampleService
-import org.jboss.modules.Module
 
 class MainApp(private val moduleService: ModuleService = JBossModuleServiceImpl()) {
     private fun loadImplementationFromServiceLoader(clazz: Class<SampleService>) {
@@ -16,11 +15,6 @@ class MainApp(private val moduleService: ModuleService = JBossModuleServiceImpl(
     }
 
     companion object {
-        private const val SUB_MODULE_1_PATH = "/Users/ukohlmeyer/projects/geodemodules/sub-module1/target/sub-module1-1.0-SNAPSHOT.jar"
-        private const val SUB_MODULE_2_PATH = "/Users/ukohlmeyer/projects/geodemodules/sub-module2/target/sub-module2-1.0-SNAPSHOT.jar"
-        private const val SUB_MODULE_3_PATH = "/Users/ukohlmeyer/projects/geodemodules/sub-module3/target/sub-module3-1.0-SNAPSHOT.jar"
-        private const val SUB_MODULE_4_PATH = "/Users/ukohlmeyer/projects/geodemodules/sub-module4/target/sub-module4-1.0-SNAPSHOT.jar"
-        private const val SUB_MODULE_5_PATH = "/Users/ukohlmeyer/projects/geodemodules/sub-module5/target/sub-module5-1.0-SNAPSHOT.jar"
         private const val SPRING_MODULE_PATH = "/Users/ukohlmeyer/projects/geodemodules/spring-submodule/target/spring-submodule-1.0-SNAPSHOT.jar"
         private const val SPRING_MODULE_2_PATH = "/Users/ukohlmeyer/projects/geodemodules/spring-submodule2/target/spring-submodule2-1.0-SNAPSHOT.jar"
 
@@ -28,11 +22,6 @@ class MainApp(private val moduleService: ModuleService = JBossModuleServiceImpl(
         fun main(args: Array<String>) {
             val mainApp = MainApp()
 
-            mainApp.registerModuleFromJar(SUB_MODULE_1_PATH, "submodule1")
-            mainApp.registerModuleFromJar(SUB_MODULE_2_PATH, "submodule2")
-            mainApp.registerModuleFromJar(SUB_MODULE_3_PATH, "submodule3")
-            mainApp.registerModuleFromJar(SUB_MODULE_4_PATH, "submodule4")
-            mainApp.registerModuleFromJar(SUB_MODULE_5_PATH, "submodule5", "submodule4")
             mainApp.registerModuleFromJar(SPRING_MODULE_PATH, "springModule")
             mainApp.registerModuleFromJar(SPRING_MODULE_2_PATH, "springModule2")
 
